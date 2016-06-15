b3a36175a6c95ecf559d2f960c9ca9a517c23126
nsaicharan <saicharan13@gmail.com>
2016-06-16 01:03:43 +0530

    Add files via upload

---
+++ parusworld/contact/messagesent.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact:Message Sent</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/thanks.css">
+	<link rel="stylesheet" href="../css/animate.css">
+    <link href='https://fonts.googleapis.com/css?family=Mate+SC:400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
+   
+</head>
+<body>
+
+<header>
+        <img src="../img/logo.png" alt="" class="logo img-responsive">
+  
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../About/index.html">About</a></li>
+                <li><a href="../Photos/index.html">Photos</a></li>
+                <li><a href="index.html">Contact</a></li>
+            </ul>
+        </nav>
+</header>
+
+<div class="container">
+    <div class="row">
+		<div class="col-sm-12" style="text-align:center;">
+			<h2>Message Sent!</h2>
+			<p class="animated fadeIn"> Thanks for contacting.</p>
+			<p class=" p2 animated fadeIn">You can also contact Sonarika via social networks.</p>
+		</div>
+	</div>
+</div>
+
+<footer>
+    <p>Copyrights &copy; <a href="http://saicharan.com">www.saicharan.com</a></p>
+</footer>
+
+</body>
+</html>
