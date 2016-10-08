979ed42b9b1d865d4bfba61aa3efaa35d2c95697
nsaicharan <saicharan13@gmail.com>
2016-10-09 00:11:20 +0530

    100+

--- new3/jquery2.js
+++ new3/jquery2.js
@@ -117,8 +117,8 @@
 
     // Default value is a center
     var args = str.split(/\s+/);
-    var x = '50%';
-    var y = '50%';
+    var x = '100%';
+    var y = '100%';
     var len;
     var arg;
     var i;
