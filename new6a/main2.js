5a5882aa39412b04c028ef385ef6d0fdbec64683
Sai <saicharan13@gmail.com>
2018-09-08 17:34:49 +0530

    Change hero text styles

---
+++ new6a/main2.js
@@ -0,0 +1,19 @@
+const isMobile = () => {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  }
+  return false;
+};
+
+if (!isMobile()) {
+  document.querySelector("#hero-video").style.display = "block";
+  document.querySelector("#hero-video").style.opacity = 1;
+}
